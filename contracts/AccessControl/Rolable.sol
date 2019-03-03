64428184fd885614e626da3b4e2400e4a8d5e551
khalidfsh <dev.khalid@me.com>
2019-03-03 09:48:12 +0300

    Reconstruct `Rolable.sol`

--- contracts/AccessControl/Rolable.sol
+++ contracts/AccessControl/Rolable.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+/// Import all Roles
 import "./Roles/Consumer.sol";
 import "./Roles/Designer.sol";
 import "./Roles/Distributor.sol";
@@ -7,12 +8,14 @@ import "./Roles/Manufacturer.sol";
 import "./Roles/Regulator.sol";
 import "./Roles/Retailer.sol";
 
+
+/// @author Khalid F.Sh
+/// @title Role Ability Contract 
+/// @dev contract for adopting all roles in access control
 contract Rolable is Consumer, Designer, Distributor, Regulator, Retailer {
     
-    constructor () public {
-
-    }
-
+    /// Function to check all roles for the caller
+    /// @return array contining every role with its state as boolean
     function whoAmI() public view returns(
         bool consumer, 
         bool retailer, 
