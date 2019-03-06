38b4cdd67c979ec27e5b25be9e5eae356a263d2a
khalidfsh <dev.khalid@me.com>
2019-03-06 07:38:32 +0300

    Add storage struct, mapping, variable, enum

--- contracts/Base/Storage/Drug.sol
+++ contracts/Base/Storage/Drug.sol
@@ -5,4 +5,53 @@ pragma solidity ^0.5.0;
 /// @title Drug Chain Contract
 contract Drug {
 
+    /// Variable for tracking Product Kepping Unit (PKU)
+    uint pku;
+
+    /// Variable for tracking Stock Louding Uint (SLU)
+    uint slu;
+
+    /// Public mapping from PKU to a Drug Item
+    mapping (uint => DrugItem) dItems;
+
+    /// Public mapping from SLU to a array of PKU
+    mapping (uint => uint[]) stockLouds;
+
+    // Enumaration for defining variety of Drug State
+    enum DrugState {
+        Manufactured,             
+        Packed,
+        ForSale,
+        Sold,
+        Shipped,
+        ShippmentEnvUpdated,
+        Received,
+        StockEnvUpdated,
+        Purchased
+    }
+
+    // Structure for Envirument update unit
+    struct EnvUpdateOpj {
+        uint timeStamp;
+        uint humidity;
+        uint temprture;
+        address updaterAddress;
+    }
+
+    /// Structure for keeping Drug Design fields structured
+    struct DrugItem {
+        uint udpc;
+        uint pku;
+        uint slu;
+        DrugState state;
+        address payable currentOwnerId;
+        address manufacturerId;
+        address deistributorId;
+        address retailerId;
+        uint price;
+        uint packingTimeStamp;
+        mapping(uint => EnvUpdateOpj) envHistory;
+        uint envUpdateCounter;
+    }
+
 }
--- contracts/Base/Storage/DrugDesign.sol
+++ contracts/Base/Storage/DrugDesign.sol
@@ -5,4 +5,45 @@ pragma solidity ^0.5.0;
 /// @title Drug Design Chain Contract
 contract DrugDesign {
     
+    /// Variable for tracking Universal Drug Product Code (UDPC)
+    uint udpc;
+
+    /// Public mapping from UDPC to a Drug Design Item
+    mapping (uint => DrugDesignItem) dDItems;
+
+    /// Enumaration for defining variety of Drug Design State
+    enum DrugDesignState {
+        Owned,             
+        Tested,
+        Approved,
+        ForSale
+    }
+
+    /// Structure keeping meta data for a Drug Design **TODO**
+    struct DrugDesignMeta {
+        string name;
+        string description;
+        string notes;
+    }
+
+    /// Structure for Drug Design Test Case **TODO**
+    struct DrugDesignTestCase {
+        address testerId;
+        uint timeStamp;
+        bool isPassed;
+        string description;
+        string notes;
+    }
+
+    /// Structure for keeping Drug Design fields structured
+    struct DrugDesignItem {
+        uint udpc;
+        address payable owner;
+        string designerName;
+        DrugDesignState state;
+        DrugDesignMeta metaData;
+        uint salePrice;
+        uint testIndexed;
+        mapping(uint => DrugDesignTestCase) testCases;
+    }
 }
