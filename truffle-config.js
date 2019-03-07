b8302dd9e93abf36a480c0cab559d02222f2bb1e
khalidfsh <dev.khalid@me.com>
2019-03-07 07:55:29 +0300

    Fetch exact version of solc and more limit of gas

--- truffle-config.js
+++ truffle-config.js
@@ -46,8 +46,8 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-      //gas: 50000000,
-      //gasPrice: 1000000000,
+      gas: 0x1fffffffffffff,
+      //gasPrice: 30000000000,
     },
 
     // Another network with more advanced options...
@@ -87,7 +87,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
