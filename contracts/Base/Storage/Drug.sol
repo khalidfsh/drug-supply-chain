fb6fe5dab5abd807d04fb356de16a6b62391b10d
khalidfsh <dev.khalid@me.com>
2019-03-08 06:20:50 +0300

    Add fetching data public functions+ Del unneeded states

--- contracts/Base/Storage/Drug.sol
+++ contracts/Base/Storage/Drug.sol
@@ -24,9 +24,7 @@ contract Drug {
         ForSale,
         Sold,
         Shipped,
-        ShippmentEnvUpdated,
         Received,
-        StockEnvUpdated,
         Purchased
     }
 
@@ -45,8 +43,8 @@ contract Drug {
         uint slu;
         DrugState state;
         address payable currentOwnerId;
-        address payable manufacturerId;
-        address payable deistributorId;
+        address manufacturerId;
+        address deistributorId;
         address retailerId;
         uint price;
         uint packingTimeStamp;
@@ -280,7 +278,6 @@ contract Drug {
             uint _pku = stockLouds[_slu][i];
 
             dItems[_pku].state = DrugState.Received;
-            dItems[_pku].currentOwnerId = msg.sender;
         }
         emit Received(_slu);
     }
@@ -325,6 +322,129 @@ contract Drug {
         dItems[_pku].currentOwnerId = msg.sender;
 
         emit Purchased(_pku);
+    }
+    
+    /// public data featching functions
+    function fetchDrugLoaudData(uint _slu) 
+        public 
+        view 
+        returns(
+            uint quantity,
+            uint _udpc,
+            string memory state,
+            address currentOwner,
+            address manufacturerId,
+            address deistributorId,
+            address retailerId,
+            uint price
+        )
+    {
+        require(_slu <= slu && _slu != 0, 'Given SLU Not Created Yet!');
+
+        uint sampleItemPKU = stockLouds[_slu][stockLouds[_slu].length-1];
+        (
+            _udpc,
+            ,
+            state,
+            currentOwner,
+            manufacturerId,
+            deistributorId,
+            retailerId,
+            price,
+            ,
+            
+        ) = fetchDrugItemData(sampleItemPKU);
+
+        quantity = stockLouds[_slu].length;
+    }
+
+    function fetchDrugItemData(uint _pku)
+        public
+        view
+        returns(
+            uint _udpc,
+            uint _slu,
+            string memory state,
+            address currentOwner,
+            address manufacturerId,
+            address deistributorId,
+            address retailerId,
+            uint price,
+            uint packingTimeStamp,
+            uint numberOfEnvUpdate
+        )
+    {
+        require(_pku <= pku && _pku != 0, 'Given PKU Not Created Yet!');
+        DrugItem  storage _drugItem = dItems[_pku];
+
+        _udpc = _drugItem.udpc;
+        _slu = _drugItem.slu;
+
+        if (_drugItem.state == DrugState.Manufactured)
+            state = 'Manufactured';
+        else if (_drugItem.state == DrugState.Packed)
+            state = 'Packed';
+        else if (_drugItem.state == DrugState.ForSale)
+            state = 'ForSale';
+        else if (_drugItem.state == DrugState.Sold)
+            state = 'Sold';
+        else if (_drugItem.state == DrugState.Shipped)
+            state = 'Shipped';
+        else if (_drugItem.state == DrugState.Received)
+            state = 'Received';
+        else if (_drugItem.state == DrugState.Purchased)
+            state = 'Purchased';
+
+        currentOwner = _drugItem.currentOwnerId;
+        manufacturerId = _drugItem.manufacturerId;
+        deistributorId = _drugItem.deistributorId;
+        retailerId = _drugItem.retailerId;
+        price = _drugItem.price;
+        packingTimeStamp = _drugItem.packingTimeStamp;
+        numberOfEnvUpdate = _drugItem.envUpdateCounter;
+    }
+
+    function fetchLoudPKUs(uint _slu)
+        public
+        view
+        returns(
+            uint[] memory
+        )
+    {
+        return stockLouds[_slu];
+    }
+
+    function fetchEnvHistory(uint _pku)
+        public
+        view
+        isDrugEnvTracked(_pku)
+        returns(
+            uint numberOfupdate,
+            uint[] memory timeStamps,
+            uint[] memory temprtures,
+            uint[] memory humiditys,
+            address[] memory updaterAddresses
+        )
+    {
+        DrugItem  storage _drugItem = dItems[_pku];
+        numberOfupdate = _drugItem.envUpdateCounter;
+        uint[] memory _timeStamps = new uint[](numberOfupdate);
+        uint[] memory _temprtures = new uint[](numberOfupdate);
+        uint[] memory _humiditys = new uint[](numberOfupdate);
+        address[] memory _updaterAddresses = new address[](numberOfupdate);
+        for (uint i = 0; i < _drugItem.envUpdateCounter; i++) {
+            EnvUpdateOpj storage _envUpdate = _drugItem.envHistory[i];
+            _timeStamps[i] = _envUpdate.timeStamp;
+            _temprtures[i] = _envUpdate.temprture;
+            _humiditys[i] = _envUpdate.humidity;
+            _updaterAddresses[i] = _envUpdate.updaterAddress;
+        }
+        (timeStamps, temprtures, humiditys, updaterAddresses) = (
+            _timeStamps, 
+            _temprtures, 
+            _humiditys,
+            _updaterAddresses
+        );
     }
 
 }
