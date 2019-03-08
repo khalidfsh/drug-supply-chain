a7bf42143910fd89c61c62eb9e8a1d910d8e6a57
khalidfsh <dev.khalid@me.com>
2019-03-08 06:26:03 +0300

    Add tests for BASE (Drug&DrugDesign) and CORE

--- test/supplychain.js
+++ test/supplychain.js
@@ -1,7 +1,8 @@
 const MainChain = artifacts.require('./Core/MainChain.sol')
 const assert = require("chai").assert;
 const truffleAssert = require('truffle-assertions');
-
+const BN = require('bn.js');
+ 
 contract('Supplychain Contracts:', async (accounts) => {
     let supplyChain
     const Deployer = accounts[0]
@@ -10,6 +11,7 @@ contract('Supplychain Contracts:', async (accounts) => {
     const Manufacturer = accounts[3]
     const Manufacturer_Owner = accounts[4]
     const Manufacturer_Partner = accounts[5]
+    const Manufacturer_Empty = accounts[6]
     const Distributor = accounts[6]
     const Retailer = accounts[7]
     const Consumer = accounts[8]
@@ -44,6 +46,29 @@ contract('Supplychain Contracts:', async (accounts) => {
         notes: 'Empty',
     }
 
+    const DrugsLoaud = {
+        slu: 1,
+        quantity: 10,
+        unitPrice: web3.utils.toWei('.2', "ether"),
+    }
+
+    const DrugItem = {
+        pku: 1,
+        price: web3.utils.toWei('.2', "ether"),
+    }
+    
+    const EnvObject1 ={
+        humidity: 15,
+        temprture: 11,
+    }
+    const EnvObject2 ={
+        humidity: 40,
+        temprture: 18,
+    }
+    const EnvObject3 ={
+        humidity: 25,
+        temprture: 13,
+    }
 
 
         
@@ -413,5 +438,334 @@ contract('Supplychain Contracts:', async (accounts) => {
 
     });
 
+    it("BASE: Drug: Only authorize manufacturer can add drug loaud, event `Manufactured` emitted, `manufacturDrugsLoud()`", async() => {
+        await supplyChain.assignMeAsManufacturer({from: Manufacturer_Empty})
+        await truffleAssert.fails(
+            supplyChain.manufacturDrugsLoud(
+                DrugsLoaud.slu,
+                DrugsLoaud.quantity,
+                {from: Manufacturer_Empty}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+
+        let tx = await supplyChain.manufacturDrugsLoud(
+            DrugDesign1.udpc,
+            DrugsLoaud.quantity,
+            {from: Manufacturer_Partner}
+        )
+        truffleAssert.eventEmitted(tx, 'Manufactured', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.state, 'Manufactured')
+        assert.equal(DrugsLoadData.quantity, DrugsLoaud.quantity)
+        assert.equal(DrugsLoadData.currentOwner, Manufacturer_Partner)
+        assert.equal(DrugsLoadData.manufacturerId, Manufacturer_Partner)
+    });
+
+    it("BASE: Drug: Only orignal manufacturer can pack drug loaud, event `Packed` emitted, `packDrugsLoud()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.packDrugsLoud(
+                DrugsLoaud.slu,
+                {from: Manufacturer_Owner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx = await supplyChain.packDrugsLoud(
+            DrugsLoaud.slu,
+            {from: Manufacturer_Partner}
+        )
+        truffleAssert.eventEmitted(tx, 'Packed', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+        
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.state, 'Packed')
+    });
+
+    it("BASE: Drug: Only orignal manufacturer can add drug loaud, event `ForSale` emitted, `addDrugsLoud()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.addDrugsLoud(
+                DrugsLoaud.slu,
+                DrugsLoaud.unitPrice,
+                {from: Manufacturer_Owner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx = await supplyChain.addDrugsLoud(
+            DrugsLoaud.slu,
+            DrugsLoaud.unitPrice,
+            {from: Manufacturer_Partner}
+        )
+        truffleAssert.eventEmitted(tx, 'ForSale', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.state, 'ForSale')
+        assert.equal(DrugsLoadData.price, DrugsLoaud.unitPrice)
+    });
+
+    it("BASE: Drug: Only distributor can buy drug loaud, event `Sold` emitted, `buyDrugsLoud()`", async() => {
+        let totalPrice = new BN(DrugsLoaud.unitPrice).mul(new BN(DrugsLoaud.quantity))
+        let balanceOfOwnerBefore = new BN(await web3.eth.getBalance(Manufacturer_Owner))
+        let balanceOfManufacturerBefore = new BN(await web3.eth.getBalance(Manufacturer_Partner))
+
+        await truffleAssert.fails(
+            supplyChain.buyDrugsLoud(
+                DrugsLoaud.slu,
+                Retailer,
+                {from: Retailer, value: totalPrice}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+
+        let tx = await supplyChain.buyDrugsLoud(
+            DrugsLoaud.slu,
+            Retailer,
+            {from: Distributor, value: totalPrice}
+        )
+        truffleAssert.eventEmitted(tx, 'Sold', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+        let balanceOfOwnerAfter = new BN(await web3.eth.getBalance(Manufacturer_Owner)).toString()
+        let balanceOfManufacturerAfter = new BN(await web3.eth.getBalance(Manufacturer_Partner))
+
+        let shareOfManufacturer = totalPrice.mul(new BN(DrugDesign1.partnerShares)).div(new BN(100))
+        let shareOfOwner = totalPrice.sub(shareOfManufacturer) 
+        
+        let expectingBalanceOfOwner = balanceOfOwnerBefore.add(shareOfOwner).toString()
+        let expectingBalanceOfManuf = balanceOfManufacturerBefore.add(shareOfManufacturer).toString()
+
+        assert.equal((balanceOfManufacturerAfter), expectingBalanceOfManuf)
+        assert.equal((balanceOfOwnerAfter), expectingBalanceOfOwner)
+
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.currentOwner, Distributor)
+        assert.equal(DrugsLoadData.deistributorId, Distributor)
+        assert.equal(DrugsLoadData.state, 'Sold')
+        assert.equal(DrugsLoadData.retailerId, Retailer)
+    });
+
+
+    it("BASE: Drug: Only orignal manufacturer can ship drug loaud to known retialer, event `Shipped` emitted, `shipDrugsLoud()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.shipDrugsLoud(
+                DrugsLoaud.slu,
+                {from: Manufacturer_Owner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx = await supplyChain.shipDrugsLoud(
+            DrugsLoaud.slu,
+            {from: Manufacturer_Partner}
+        )
+        truffleAssert.eventEmitted(tx, 'Shipped', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.state, 'Shipped')
+    });
+
+    it("BASE: Drug: Only orignal manufacturer and distributor of drug loaud can update environment, event `EnvUpdated` emitted, `updateDrugsLoudShippmentEnv()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.updateDrugsLoudShippmentEnv(
+                DrugsLoaud.slu,
+                EnvObject1.humidity,
+                EnvObject1.temprture,
+                {from: Manufacturer_Owner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx1 = await supplyChain.updateDrugsLoudShippmentEnv(
+            DrugsLoaud.slu,
+            EnvObject1.humidity,
+            EnvObject1.temprture,
+            {from: Manufacturer_Partner}
+        )
+        truffleAssert.eventEmitted(tx1, 'EnvUpdated', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+        let tx2 = await supplyChain.updateDrugsLoudShippmentEnv(
+            DrugsLoaud.slu,
+            EnvObject2.humidity,
+            EnvObject2.temprture,
+            {from: Distributor}
+        )
+        truffleAssert.eventEmitted(tx2, 'EnvUpdated', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+        let DrugItemPKUs = await supplyChain.fetchLoudPKUs.call(DrugsLoaud.slu);
+        let DrugEnvHistory = await supplyChain.fetchEnvHistory.call(DrugItemPKUs[0]);
+
+        assert.equal(DrugEnvHistory.humiditys[0], EnvObject1.humidity)
+        assert.equal(DrugEnvHistory.temprtures[0], EnvObject1.temprture)
+        assert.equal(DrugEnvHistory.updaterAddresses[0], Manufacturer_Partner)
+
+        assert.equal(DrugEnvHistory.humiditys[1], EnvObject2.humidity)
+        assert.equal(DrugEnvHistory.temprtures[1], EnvObject2.temprture)
+        assert.equal(DrugEnvHistory.updaterAddresses[1], Distributor)
+    });
+
+
+    it("BASE: Drug: Only retiler was chosen by distributor can receive drug loaud, event `Received` emitted, `receiveDrugsLoud()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.receiveDrugsLoud(
+                DrugsLoaud.slu,
+                {from: Manufacturer_Partner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx = await supplyChain.receiveDrugsLoud(
+            DrugsLoaud.slu,
+            {from: Retailer}
+        )
+        truffleAssert.eventEmitted(tx, 'Received', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+        let DrugsLoadData = await supplyChain.fetchDrugLoaudData.call(DrugsLoaud.slu);
+
+        assert.equal(DrugsLoadData.state, 'Received')
+    });
+
+    it("BASE: Drug: Only retailer of drug loaud after recive can update environment, event `EnvUpdated` emitted, `updateDrugsLoudStockEnv()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.updateDrugsLoudStockEnv(
+                DrugsLoaud.slu,
+                EnvObject3.humidity,
+                EnvObject3.temprture,
+                {from: Manufacturer_Partner}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+        let tx1 = await supplyChain.updateDrugsLoudStockEnv(
+            DrugsLoaud.slu,
+            EnvObject3.humidity,
+            EnvObject3.temprture,
+            {from: Retailer}
+        )
+        truffleAssert.eventEmitted(tx1, 'EnvUpdated', (ev) => {
+            assert.equal(ev.slu, DrugsLoaud.slu)
+            return true
+        })
+
+        let DrugItemPKUs = await supplyChain.fetchLoudPKUs.call(DrugsLoaud.slu);
+        let DrugEnvHistory = await supplyChain.fetchEnvHistory.call(DrugItemPKUs[0]);
+
+        assert.equal(DrugEnvHistory.humiditys[2], EnvObject3.humidity)
+        assert.equal(DrugEnvHistory.temprtures[2], EnvObject3.temprture)
+        assert.equal(DrugEnvHistory.updaterAddresses[2], Retailer)
+    });
+
+
+    it("BASE: Drug: Only consumer can buy drug loaud, event `Purchased` emitted, `purchaseDrug()`", async() => {
+        let drugPrice = new BN(DrugItem.price)
+
+        let balanceOfDistrBefore = new BN(await web3.eth.getBalance(Distributor))
+        let balanceOfRetaiBefore = new BN(await web3.eth.getBalance(Retailer))
+        let balanceOfDeploBefore = new BN(await web3.eth.getBalance(Deployer))
+
+        await truffleAssert.fails(
+            supplyChain.purchaseDrug(
+                DrugItem.pku,
+                {from: Manufacturer_Owner, value: drugPrice}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+
+        let tx = await supplyChain.purchaseDrug(
+            DrugItem.pku,
+            {from: Consumer, value: drugPrice}
+        )
+        truffleAssert.eventEmitted(tx, 'Purchased', (ev) => {
+            assert.equal(ev.pku, DrugItem.pku)
+            return true
+        })
+
+        let balanceOfDistrAfter = new BN(await web3.eth.getBalance(Distributor)).toString()
+        let balanceOfRetaiAfter = new BN(await web3.eth.getBalance(Retailer)).toString()
+        let balanceOfDeplyAfter = new BN(await web3.eth.getBalance(Deployer)).toString()
+
+        let shareOfRetailer = drugPrice.mul(new BN(5)).div(new BN(100))
+        let shareOfDeveloper = drugPrice.mul(new BN(1)).div(new BN(100))
+        let shareOfDistributor = drugPrice.sub(shareOfRetailer).sub(shareOfDeveloper)
+
+        
+        let expectingBalanceOfDistr = balanceOfDistrBefore.add(shareOfDistributor).toString()
+        let expectingBalanceOfRetia = balanceOfRetaiBefore.add(shareOfRetailer).toString()
+        let expectingBalanceOfDeplo = balanceOfDeploBefore.add(shareOfDeveloper).toString()
+
+
+        assert.equal((balanceOfDistrAfter), expectingBalanceOfDistr)
+        assert.equal((balanceOfRetaiAfter), expectingBalanceOfRetia)
+        assert.equal((balanceOfDeplyAfter), expectingBalanceOfDeplo)
+
+
+        let DrugData = await supplyChain.fetchDrugItemData.call(DrugItem.pku);
+        assert.equal(DrugData.currentOwner, Consumer)
+        assert.equal(DrugData.state, 'Purchased')
+    });
+
+    it("CORE: Pausable: Only pauser role `deployer` of this contract can pause supply chain , `pause()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.pause({from: Manufacturer_Partner}),
+            truffleAssert.ErrorType.REVERT
+        )
+        await supplyChain.pause({from: Deployer})
+
+        let isPaused = await supplyChain.paused.call()
+        assert.isTrue(isPaused)
+    });
+
+    it("CORE: Pausable: Designr cannot add new drug after contract paused , `whenNotPaused()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.designDrug(
+                DrugDesign2.designerName, 
+                DrugDesign2.name, 
+                DrugDesign2.description, 
+                DrugDesign2.notes, 
+                {from: Designer}
+            ),
+            truffleAssert.ErrorType.REVERT
+        )
+    });
+
+    it("CORE: Ownable: Only owner`deployer` of this contract can self destruct , `kill()`", async() => {
+        await truffleAssert.fails(
+            supplyChain.kill({from: Manufacturer_Partner}),
+            truffleAssert.ErrorType.REVERT
+        )
+        await supplyChain.kill({from: Deployer})
+    });
+
+   
 
 });
+
